f12ed1c78bcfefa1909d71c8269481cad1bcded5
Tembo Madalitso <tembomadalito9@gmail.com>
2024-12-27 07:18:30 +0200

    About-done

--- MyPortifolio/js/script.js
+++ MyPortifolio/js/script.js
@@ -1,6 +1,15 @@
-function toggleMenu() {
-    var nav = document.getElementById("nav_bar");
-    var hamburger = document.getElementById("hamburger");
-    nav.classList.toggle("active");
-    hamburger.classList.toggle("hidden");  // Hide the hamburger icon
-}
+ var tablinks = document.getElementsByClassName("tab-links");
+ var tabcontents = document.getElementsByClassName("tab-content");
+
+ function opentab(tabname) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+ }

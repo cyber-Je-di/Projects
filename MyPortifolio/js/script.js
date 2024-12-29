eea9b8761546edce575cab4db8a9e8a03fdcd908
Tembo Madalitso <tembomadalito9@gmail.com>
2024-12-29 14:12:54 +0200

    UI-done(copyright needs modification)

--- MyPortifolio/js/script.js
+++ MyPortifolio/js/script.js
@@ -1,5 +1,6 @@
  var tablinks = document.getElementsByClassName("tab-links");
  var tabcontents = document.getElementsByClassName("tab-content");
+ var sidemenu = document.getElementById("sidemenu");
 
  function opentab(tabname) {
     for(tablink of tablinks) {
@@ -13,3 +14,11 @@
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
  }
+
+ function openmenu() {
+    sidemenu.style.right = "0";
+ }
+
+ function closemenu() {
+    sidemenu.style.right = "-500px";
+  }

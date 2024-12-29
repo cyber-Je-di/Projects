e0fb43323ec5e130ec851de4aead88bd58befd2a
Tembo Madalitso <tembomadalito9@gmail.com>
2024-12-29 20:25:12 +0200

    working-protoptype

--- MyPortifolio/js/script.js
+++ MyPortifolio/js/script.js
@@ -1,24 +1,41 @@
- var tablinks = document.getElementsByClassName("tab-links");
- var tabcontents = document.getElementsByClassName("tab-content");
- var sidemenu = document.getElementById("sidemenu");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-content");
+var sidemenu = document.getElementById("sidemenu");
 
- function opentab(tabname) {
-    for(tablink of tablinks) {
+function opentab(tabname) {
+    for (let tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
 
-    for(tabcontent of tabcontents) {
+    for (let tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
- }
+}
 
- function openmenu() {
+function openmenu() {
     sidemenu.style.right = "0";
- }
+}
 
- function closemenu() {
+function closemenu() {
     sidemenu.style.right = "-500px";
-  }
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwiLWrrw0vHJibDJbfX6gKhZKuHJh0Y3_RC3Mq18NLV1C7xE2sqJ3DU_PZ5CdCFg5Fn/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById('msg');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        }) // Close the .then block properly
+        .catch(error => console.error('Error!', error.message));
+});
